1e7de8e397e80d34b26323fba8234bea9c76c9d2
nakuwa23 <b.nakuwa@alustudent.com>
2025-11-29 21:28:53 +0200

    Strace concept

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix Apache 500 error caused by incorrect file extension in WordPress settings
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wordpress/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
 
