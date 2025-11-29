2e0b5a8a102808bb126c907468312a3e35ca9443
nakuwa23 <b.nakuwa@alustudent.com>
2025-11-29 22:03:41 +0200

    Fix holberton user file descriptor limits

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,11 @@
 # Script that changes OS configuration to allow holberton user to open files without errors
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/" /etc/security/limits.conf && sed -i "s/holberton soft nofile 4/holberton soft nofile 50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/'
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
